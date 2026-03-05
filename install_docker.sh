d18d8556358d3bd51ab3c7808286a7800a1ca0f9
Syam Sarosa <syamsarosa@gmail.com>
2026-03-06 00:24:26 +0700

    Install Docker | Change ECHO to echo

--- install_docker.sh
+++ install_docker.sh
@@ -15,38 +15,38 @@ fi
 ##############################################
 # Update server package and install required dependency
 ##############################################
-ECHO "===== INSTALLING DOCKER DEPENDENCIES ====="
+echo "===== INSTALLING DOCKER DEPENDENCIES ====="
 apt update
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 
 ##############################################
 # Create Keyring and add Docker GPG to server's keyring
 ##############################################
-ECHO "===== ADD DOCKER GPG KEYRING ====="
+echo "===== ADD DOCKER GPG KEYRING ====="
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 
 ##############################################
 # Add latest Docker repository to APT
 ##############################################
-ECHO "===== ADDING LATEST DOCKER REPOSITORY ====="
+echo "===== ADDING LATEST DOCKER REPOSITORY ====="
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 
 ##############################################
 # Install Docker
 ##############################################
-ECHO "===== INSTALLING DOCKER ====="
+echo "===== INSTALLING DOCKER ====="
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ##############################################
 # Verify Docker Installation
 ##############################################
-ECHO "===== VERIFYING DOCKER ====="
+echo "===== VERIFYING DOCKER ====="
 docker --version
 
 ##############################################
 # Add user to Docker group
 ##############################################
-ECHO "===== ADDING SUDO USER TO DOCKER GROUP ====="
+echo "===== ADDING SUDO USER TO DOCKER GROUP ====="
 usermod -aG docker $USER
